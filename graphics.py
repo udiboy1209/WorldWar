526916722641cb05246639e2e09de07995a786f7
Meet Udeshi <mudeshi1209@gmail.com>
2015-09-17 19:53:50 +0530

    Move logic done

--- graphics.py
+++ graphics.py
@@ -11,25 +11,25 @@ pygame.init()
 WINDOW_W,WINDOW_H= (950,700)
 NODE_R = [8,10,15,20]
 
+# CONSTANTS ###############
 
-##########################
+BLANK=1
+ACTION=2
+
+PLAYER_ALLY=1
+PLAYER_AXIS=-1
+
+SELECT=1
+MOVE=2
 
 DISPLAYSURF = pygame.display.set_mode((WINDOW_W,WINDOW_H))
 pygame.display.set_caption('World War!')
 
-# COLORS #################
-
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 128)
-BLACK = (0, 0, 0)
-
-COLOR_HOVER = (128,0,128)
-COLOR_SELECTED = (0,128,128)
-
-##########################
-
 BASICFONT = pygame.font.Font(None, 20)
+LARGEFONT = pygame.font.Font(None, 40)
+LARGEFONT.set_bold(True)
+
+
 NODE_IMG = [pygame.image.load("img/node1.png"),
     pygame.image.load("img/node2.png"),
     pygame.image.load("img/node3.png"),
@@ -37,18 +37,29 @@ NODE_IMG = [pygame.image.load("img/node1.png"),
 
 BG = pygame.image.load("img/map.png")
 
-###########################
+# COLORS #################
 
-def display_text(text, color, center):
-    textSurfaceObj = BASICFONT.render(text, True, color)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 128)
+BLACK = (0, 0, 0)
+
+COLOR_VALID = (128,255,255)
+COLOR_HOVER = (128,0,128)
+COLOR_SELECTED = (0,128,128)
+
+# FUNCTIONS ###############
+
+def display_text(text, center, color=BLACK, font=BASICFONT):
+    textSurfaceObj = font.render(text, True, color)
     textRectObj = textSurfaceObj.get_rect()
     textRectObj.center = center
     DISPLAYSURF.blit(textSurfaceObj, textRectObj)
 
 def get_hovered_node(pos):
     for n in network:
-        lvl = 4-n.level if n.occupant>0 else n.level-5
-        if math.sqrt((n.x-pos[0])**2 + (n.y-pos[1])**2)<NODE_R[lvl]:
+        if math.sqrt((n.x-pos[0])**2 + (n.y-pos[1])**2)<NODE_R[n.rank]+5:
             return n
 
 # STATE DATA ##############
@@ -56,6 +67,13 @@ def get_hovered_node(pos):
 hovered=None
 selected_1=None
 selected_2=None
+
+menu_state = BLANK
+game_state = SELECT
+player = PLAYER_ALLY
+
+move_pointer=None
+num_to_move = ""
 
 ###########################
 
@@ -69,8 +87,41 @@ while True: # main game loop
             sys.exit()
         elif event.type ==  MOUSEMOTION:
             hovered=get_hovered_node(event.pos)
+            if game_state == MOVE:
+                move_pointer = event.pos
+
         elif event.type == MOUSEBUTTONUP:
-            selected_1=hovered
+            if(event.pos[0] < 750): # Clicked on Map
+                if game_state == SELECT:
+                    if hovered.occupant==player:
+                        selected_1=hovered
+                elif game_state == MOVE:
+                    if hovered in adjacent_levels(selected_1):
+                        selected_2=hovered
+
+                menu_state = ACTION if selected_1 is not None else BLANK
+            else: # Clicked on Menu
+                if menu_state == ACTION and event.pos[0]>830 and event.pos[0]<870 and event.pos[1]>170 and event.pos[1]<230:
+                    game_state=MOVE if game_state is SELECT else SELECT
+
+        elif event.type == KEYUP:
+            if menu_state == ACTION:
+                if event.key >= 48 and event.key < 58:
+                    num_to_move+=str(event.key-48)
+                elif event.key == K_BACKSPACE:
+                    num_to_move=num_to_move[:-1]
+
+        if selected_1 is not None and selected_2 is not None:
+            if num_to_move == '':
+                print "Enter a number to move"
+                selected_2 = None
+            elif move(selected_1,selected_2, int(num_to_move), 0, 0, 0):
+                selected_1=None
+                selected_2=None
+                game_state=SELECT
+                menu_state=BLANK
+                num_to_move=""
+                player = PLAYER_AXIS if player is PLAYER_ALLY else PLAYER_ALLY
 
     ################################
 
@@ -85,7 +136,10 @@ while True: # main game loop
     if selected_1 is not None:
         pygame.draw.circle(DISPLAYSURF, COLOR_SELECTED, (selected_1.x,selected_1.y), NODE_R[selected_1.rank] + 7, 5)
         for n in adjacent_levels(selected_1):
-            pygame.draw.line(DISPLAYSURF, BLACK, (selected_1.x, selected_1.y), (n.x,n.y), 2)
+            pygame.draw.aaline(DISPLAYSURF, BLACK, (selected_1.x, selected_1.y), (n.x,n.y), 2)
+        if game_state == MOVE and move_pointer is not None:
+            pygame.draw.aaline(DISPLAYSURF, RED, (selected_1.x, selected_1.y), (move_pointer[0],move_pointer[1]), 4)
+
 
     for n in network:
         nodeimg = NODE_IMG[n.rank]
@@ -93,9 +147,19 @@ while True: # main game loop
         imgRect.center = (n.x,n.y)
         DISPLAYSURF.blit(nodeimg,imgRect)
 
+        txtcol = BLACK
+        if n.occupant==PLAYER_AXIS:
+            txtcol = (128,0,0)
+        if n.occupant==PLAYER_ALLY:
+            txtcol = (0,128,0)
+
         display_text("%d" % n.number,
-                BLACK,
-                (n.x, n.y + NODE_R[n.rank]+5))
+                (n.x, n.y + NODE_R[n.rank]+5), txtcol)
+
+    if menu_state == ACTION:
+        display_text("Troops: %d" % selected_1.number, (850, 50), BLUE, LARGEFONT)
+        display_text(num_to_move, (850,100), BLACK, LARGEFONT)
+        display_text("MOVE" if game_state is SELECT else "CANCEL", (850,200), BLACK, LARGEFONT)
 
     pygame.display.update()
 
